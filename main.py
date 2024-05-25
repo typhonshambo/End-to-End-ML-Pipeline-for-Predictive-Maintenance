7d46cec966a7b40f91899749350232c27efc9622
Shambo Chowdhury <54593764+typhonshambo@users.noreply.github.com>
2024-05-26 02:57:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from pipelines.training_pipeline import training_pipe
 from pipelines.prediction_pipeline import prediction_pipeline
+import dagshub
+import mlflow
+
 if __name__ == "__main__":
+
+
+    dagshub.init("End-to-End-ML-Pipeline-for-Predictive-Maintenance", "typhonshambo", mlflow=True)
+    mlflow.start_run()
     training_pipe()
     prediction_pipeline()
 
